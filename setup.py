8e3b6f1355091d3a328fc7d04f3dd031fc729d43
Anthony Shaw <anthonyshaw@apache.org>
2018-03-04 17:32:30 +1100

    most naive setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='friendly-deprecation-test',
+    version='1.0',
+    description='Testing phasing out Python runtimes',
+    author='Anthony Shaw',
+    author_email='anthonyshaw@apache.org',
+    url='https://github.com/tonybaloney/friendly-deprecation-test',
+    packages=['friends'],
+)
