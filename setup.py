97c43311479730d9fa8bfc2256fabe72ae61c383
Anthony Shaw <anthonyshaw@apache.org>
2018-03-04 17:39:27 +1100

    add some classifiers

--- setup.py
+++ setup.py
@@ -9,4 +9,12 @@ setup(
     author_email='anthonyshaw@apache.org',
     url='https://github.com/tonybaloney/friendly-deprecation-test',
     packages=['friends'],
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
 )
