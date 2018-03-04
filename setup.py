ec89444366a47b7c008359c2fe7e6606c06e1ad2
Anthony Shaw <anthonyshaw@apache.org>
2018-03-04 18:06:34 +1100

    bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='friendly-deprecation-test',
-    version='1.4.0',
+    version='1.5.0',
     description='Testing phasing out Python runtimes',
     author='Anthony Shaw',
     author_email='anthonyshaw@apache.org',
